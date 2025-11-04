000dd6388d59e17cc669d9e7ecb6ad081b619aa9
shokh0126mardonov <shokh0126mardonov@example.com>
2025-11-04 13:35:47 +0500

    todos

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import home_view
+from .views import home_view,tasks_view,add_task_view
+
+app_name = 'todos'
+
 
 urlpatterns = [
-    path('',home_view,name='home_page')
+    path('',home_view,name='home_page'),
+    path('tasks/',tasks_view,name='tasks_page'),
+    path('add/',add_task_view,name='add_page'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 from django.http import HttpRequest,HttpResponse
 
-# Create your views here.
+todos = []
+
+
 def home_view(request:HttpRequest)->HttpResponse:
-    return HttpResponse('Hello world')
+    return render(
+        request=request,
+        template_name='home.html'
+    )
+
+
+def tasks_view(request:HttpRequest)->HttpResponse:
+    return render(
+        request=request,
+        template_name='tasks.html',
+        context={
+            'todos':todos
+            }
+    )
+
+def add_task_view(request:HttpRequest)->HttpResponse:
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+
+        new_todo = {
+            'title':title,
+            'description':description
+        }
+        todos.append(new_todo)
+        print(todos)
+        return redirect(reverse('todos:tasks_page'))
+    else:
+        return render(
+        request=request,
+        template_name='add_task.html',
+    )
